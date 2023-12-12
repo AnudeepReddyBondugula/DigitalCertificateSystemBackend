ef84867acedf9477629cb83681a4f3c2f76d5fb4
Anudeep Reddy Bondugula <anudeepreddybondugula@gmail.com>
2023-12-12 20:02:51 +0530

    Implemented Token Verification Middleware for Private Routes

--- src/controllers/Authentication/verifyToken.js
+++ src/controllers/Authentication/verifyToken.js
@@ -1,25 +1,4 @@
 const jwt = require("jsonwebtoken");
-// const verifyToken = async (req, res, next) => {
-//     try{
-//         const token = req.headers.authorization;
-	  
-//         if (!token) {
-//           return res.status(401).json({ message: "Unauthorized" });
-//         }
-	  
-//         jwt.verify(token, process.env.SECRET, async (err, decoded) => {
-//           if (err) {
-//             return res.status(401).json({ message: "Unauthorized" });
-//           }
-//           req.body.role = decoded.role;
-//           req.body.username = decoded.username;
-//           next();
-//         });
-//     }
-//     catch (err){
-//         return res.status(500).json({message : "Internal Server Error"});
-//     }
-//   };
 
 const verifyToken = async (token, secret) => {
 	
