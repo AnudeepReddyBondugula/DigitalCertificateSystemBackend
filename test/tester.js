8fb35318a6ca6e2afc75b128d0f7caeaf0da9693
Anudeep Reddy Bondugula <anudeepreddybondugula@gmail.com>
2024-01-05 12:10:10 +0530

    Updated

--- test/tester.js
+++ test/tester.js
@@ -1,5 +1,11 @@
+const {getData, saveData} = require("../src/services/IpfsManager")
+
 async function main () {
-    
+    const data = "Hello World!";
+    const cid = await saveData(data);
+    console.log(`"CID : ${cid}`)
+    const result = await getData(cid);
+    console.log(`Result: ${result}`);
 }
 
 main();
